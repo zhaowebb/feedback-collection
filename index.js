0c65fca01982040c7d12dea40137c827fe613a74
zhaowebb <zhaowebb@gmail.com>
2017-11-12 20:53:01 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3333
+
+
+app.get('/', (req, res) => {
+	res.send({hi: 'there'})
+})
+
+
+app.listen(3333)
